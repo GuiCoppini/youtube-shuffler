23397a759d9e5de6d2ac7547e74b423d368a3f36
Coppini <hozerboy2239@gmail.com>
2018-07-20 22:18:00 -0300

    Put audio files in the right place and print their bytes

--- src/main/kotlin/shuffler/youtubeshuffler/controller/TestController.kt
+++ src/main/kotlin/shuffler/youtubeshuffler/controller/TestController.kt
@@ -1,24 +1,34 @@
 package shuffler.youtubeshuffler.controller
 
 import org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE
+import org.springframework.util.ResourceUtils
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Mono
+import reactor.core.publisher.Flux
 import javax.servlet.http.HttpServletResponse
+
 
 @RestController
 class TestController {
 
     @RequestMapping(path = ["/"], produces = [TEXT_EVENT_STREAM_VALUE])
-    fun getString(response: HttpServletResponse): Mono<String> {
+    fun getString(response: HttpServletResponse): Flux<String> {
 //        response.contentType = "multipart/x-mixed-replace;"
-        return Mono.create {
-            var str = "a"
-            val out = response.outputStream
-            while (true) {
-                str += "a"
-                out.print(str)
-            }
+
+        val out = response.outputStream
+
+        var totalFramesRead = 0
+        val fileIn = ResourceUtils.getFile("classpath:tetris-mp3.mp3")
+
+//        val audioInputStream = AudioSystem.getAudioInputStream(fileIn)
+
+        val bytes = fileIn.readBytes()
+
+
+        return Flux.create {
+            //            while (true) {
+            bytes.forEach { out.print(it.toString()) }
+//            }
         }
     }
 
