2200256a443bfc95be2a1def85bd92eb289a4a09
Coppini <hozerboy2239@gmail.com>
2018-07-22 21:01:08 -0300

    Saves work from where I stopped

--- src/main/kotlin/shuffler/youtubeshuffler/controller/TestController.kt
+++ src/main/kotlin/shuffler/youtubeshuffler/controller/TestController.kt
@@ -1,21 +1,50 @@
 package shuffler.youtubeshuffler.controller
 
+import org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE
+import org.springframework.util.ResourceUtils
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletResponse
 
 
+@CrossOrigin
 @RestController
 class TestController {
-//    @RequestMapping(path = ["/"], produces = [TEXT_EVENT_STREAM_VALUE])
-//    fun getString(response: HttpServletResponse): Disposable {
-////        response.contentType = "multipart/x-mixed-replace;"
+
+
+    @RequestMapping(path = ["/"], produces = [TEXT_EVENT_STREAM_VALUE])
+    fun getString(response: HttpServletResponse) {
+        response.contentType = "audio/mpeg3"
+
+        println("BATERAM NO /")
+
+        val out = response.outputStream
+
+        val fileIn = ResourceUtils.getFile("classpath:tetris-mp3.mp3")
+        val fileBytes = fileIn.readBytes()
+
+        out.write(fileBytes)
+        out.flush()
+        data = inputStream.read()
+    }
+
+//    @RequestMapping(path = ["/"])
+//    fun getAudioBytes(response: HttpServletResponse) {
 //        println("BATERAM NO /")
+//        response.contentType = "audio/mpeg;"
 //
 //        val out = response.outputStream
 //
-//        return outBuffer.keepUpdatingBuffer()
-//                .doOnNext {
-//                    out.write(it)
-//                }
-//                .subscribe()
+//        val fileIn = ResourceUtils.getFile("classpath:tetrismp3.mp3")
+////        val musicBytes = fileIn.readBytes()
+//
+//        val inputStream = fileIn.readBytes().inputStream()
+////        val outStream: OutputStream = fileIn.outputStream()
+//
+//        var data = inputStream
+//        println(data)
+//        inputStream.copyTo(out)
+//        out.flush()
 //    }
 }
--- src/main/resources/static/socket.js
+++ src/main/resources/static/socket.js
@@ -1,16 +1,16 @@
 function startSocket() {
-    var socket = new WebSocket('ws://' + window.location.host + '/my-websocket-endpoint');
-
-    // Add an event listener for when a connection is open
-    socket.onopen = function() {
-      console.log('WebSocket connection opened. Ready to send messages.');
-
-      // Send a message to the server
-      socket.send('Hello, from WebSocket client!');
+    var sock = new SockJS('http://localhost:8080/my-websocket-endpoint');
+    sock.onopen = function() {
+     console.log('open');
+     sock.send('MENSAGEM PORRAN');
     };
 
-    // Add an event listener for when a message is received from the server
-    socket.onmessage = function(message) {
-      console.log('Message received from server: ' + message);
-};
+    sock.onmessage = function(e) {
+     console.log('message', e.data);
+     sock.close();
+    };
+
+    sock.onclose = function() {
+     console.log('close');
+    };
 }
