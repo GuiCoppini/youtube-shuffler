fc03858bc13d08d32d7c79defc670e0541ac6c0b
Guilherme Coppini Pavani <gpavani@uolinc.com>
2018-07-24 12:52:25 -0300

    Plays file bytes as they come

--- src/main/resources/static/myJs.js
+++ src/main/resources/static/myJs.js
@@ -22,6 +22,7 @@ function fetchBytes() {
             }
 
             // result.value for fetch streams is a Uint8Array
+            playByteArray(result.value)
             bytesReceived += result.value.length;
             console.log('Received', bytesReceived, 'bytes of data so far');
 
@@ -29,4 +30,46 @@ function fetchBytes() {
             return reader.read().then(processResult);
         });
     });
+}
+
+
+window.onload = init;
+var context;    // Audio context
+var buf;        // Audio buffer
+
+function init() {
+if (!window.AudioContext) {
+    if (!window.webkitAudioContext) {
+        alert("Your browser does not support any AudioContext and cannot play back this audio.");
+        return;
+    }
+        window.AudioContext = window.webkitAudioContext;
+    }
+
+    context = new AudioContext();
+}
+
+function playByteArray(byteArray) {
+
+    var arrayBuffer = new ArrayBuffer(byteArray.length);
+    var bufferView = new Uint8Array(arrayBuffer);
+    for (i = 0; i < byteArray.length; i++) {
+      bufferView[i] = byteArray[i];
+    }
+
+    context.decodeAudioData(arrayBuffer, function(buffer) {
+        buf = buffer;
+        play();
+    });
+}
+
+// Play the loaded file
+function play() {
+    // Create a source node from the buffer
+    var source = context.createBufferSource();
+    source.buffer = buf;
+    // Connect to the final output node (the speakers)
+    source.connect(context.destination);
+    // Play immediately
+    source.start(0);
 }
