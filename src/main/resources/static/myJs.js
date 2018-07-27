788a0d9c9f3241920ca832451d63bc824b1945f3
Guilherme Coppini Pavani <gpavani@uolinc.com>
2018-07-27 12:47:23 -0300

    myJs is now at the simplest level it could be => playing audio as they come

--- src/main/resources/static/myJs.js
+++ src/main/resources/static/myJs.js
@@ -1,36 +1,28 @@
-function fetchBytes() {
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+
+function firstFetch() {
     fetch("http://localhost:8080/").then(response => {
-        var reader = response.body.getReader();
-        var bytesReceived = 0;
 
-        // read() returns a promise that resolves
-        // when a value has been received
-        return reader.read().then(function processResult(result) {
-            if (result.done) {
-                console.log("Fetch complete");
-                return;
-            }
+      var reader = response.body.getReader();
+      var bytesReceived = 0;
 
-            playByteArray(result.value)
-            bytesReceived += result.value.length;
-            console.log('Received', bytesReceived, 'bytes of data so far');
-
-            // Read some more, and call this function again
-            return reader.read().then(processResult);
+      return reader.read().then(function processResult(result) {
+        if (result.done) {
+          console.log("Fetch complete");
+          return;
+        }
+        context.decodeAudioData(result.value.buffer, function(buffer) {
+              var source = context.createBufferSource();
+              source.buffer = buffer;
+              source.connect(context.destination);
+              source.start(0);
         });
-    });
-}
 
-function playByteArray(byteArray) {
+        bytesReceived += result.value.length;
+        console.log('Received', bytesReceived, 'bytes of data so far');
 
-    var arrayBuffer = new ArrayBuffer(byteArray.length);
-    var bufferView = new Uint8Array(arrayBuffer);
-    for (i = 0; i < byteArray.length; i++) {
-      bufferView[i] = byteArray[i];
-    }
-
-    context.decodeAudioData(arrayBuffer, function(buffer) {
-        buf = buffer;
-        play();
+        return reader.read().then(processResult);
+      });
     });
 }
