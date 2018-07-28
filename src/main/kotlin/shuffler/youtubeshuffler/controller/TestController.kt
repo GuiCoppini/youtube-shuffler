c210f192ed7f5ca8979531207b6469ba7ff73b86
Coppini <hozerboy2239@gmail.com>
2018-07-28 18:12:54 -0300

    Start using time sync strategy

--- src/main/kotlin/shuffler/youtubeshuffler/controller/TestController.kt
+++ src/main/kotlin/shuffler/youtubeshuffler/controller/TestController.kt
@@ -1,45 +1,48 @@
 package shuffler.youtubeshuffler.controller
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE
+import org.springframework.util.ResourceUtils
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import shuffler.youtubeshuffler.service.OutputBuffer
-import java.io.IOException
+import shuffler.youtubeshuffler.service.SongTimer
 import java.util.*
 import javax.servlet.http.HttpServletResponse
-
 
 @CrossOrigin
 @RestController
 class TestController {
 
+
     @Autowired
-    lateinit var outputBuffer: OutputBuffer
+    lateinit var songTimer: SongTimer
 
+    @RequestMapping(path = ["/get-song"], produces = [TEXT_EVENT_STREAM_VALUE])
+    fun getSong(response: HttpServletResponse) {
+        val out = response.outputStream
+        val fileIn = ResourceUtils.getFile("classpath:tetris-mp3.mp3")
+        val bytes = fileIn.readBytes()
 
-    @RequestMapping(path = ["/"])
-    fun getString(response: HttpServletResponse) {
-        response.contentType = "audio/mpeg"
-//        response.contentType = "application/octet-stream"
-        response.setHeader("Transfer-Encoding", "chunked")
+        out.write(bytes)
+    }
 
-        println("Comecou a tocar")
-
-        outputBuffer.mp3Stats()
-
-        if (!outputBuffer.isPlaying) {
-            outputBuffer.reactiveStartPlaying().subscribe {
-                try {
-                    response.outputStream.write(it)
-                    response.outputStream.flush()
-                    println("Escreveu ${Arrays.toString(it)}")
-                } catch (ex: IOException) {
-                    println("Browser closed connection, but song is still playing.")
-
-                }
+    @RequestMapping("/start-get-time")
+    fun startPlaying(response: HttpServletResponse) {
+        val out = response.outputStream
+        if(!songTimer.isPlaying) {
+            songTimer.startPlaying().subscribe {
+                out.println(it)
+                out.close()
             }
+        } else {
+            out.println(getTime())
+            out.close()
         }
     }
-}
 
+    @RequestMapping("/time")
+    fun getTime() : Long {
+        return songTimer.currentTime
+    }
+}
---
+++ src/main/kotlin/shuffler/youtubeshuffler/service/SongTimer.kt
@@ -0,0 +1,37 @@
+package shuffler.youtubeshuffler.service
+
+import javazoom.spi.mpeg.sampled.file.MpegAudioFileReader
+import org.springframework.stereotype.Service
+import org.springframework.util.ResourceUtils
+import reactor.core.publisher.Flux
+
+
+@Service
+class SongTimer {
+
+    val fileIn = ResourceUtils.getFile("classpath:tetris-mp3.mp3")
+    var currentTime: Long = 0
+
+    var isPlaying = false
+
+//    val songDuration =
+
+    fun startPlaying(): Flux<Long> {
+        if (!isPlaying) {
+            isPlaying = true
+            val properties = MpegAudioFileReader().getAudioFileFormat(fileIn).properties()
+            val duration: Long = properties["duration"] as Long / 1000000
+            return Flux.create<Long> {
+                while (currentTime < duration) {
+                    it.next(currentTime + 1)
+                    currentTime += 1
+                    Thread.sleep(1000)
+                }
+                isPlaying = false
+            }
+        }
+        return Flux.create {
+            it.next(currentTime)
+        }
+    }
+}
