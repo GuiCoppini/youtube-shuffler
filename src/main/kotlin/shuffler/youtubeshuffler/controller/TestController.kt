ce954ee95c271bf15be5d5b927dd473fa7852c11
Guilherme Coppini Pavani <gpavani@uolinc.com>
2018-07-31 14:47:04 -0300

    Print stuff

--- src/main/kotlin/shuffler/youtubeshuffler/controller/TestController.kt
+++ src/main/kotlin/shuffler/youtubeshuffler/controller/TestController.kt
@@ -18,19 +18,24 @@ class TestController {
 
     @RequestMapping(path = ["/get-song"])
     fun getSong(response: HttpServletResponse) {
+        println("Uploading MP3")
         val out = response.outputStream
         response.contentType = "audio/mpeg"
         val fileIn = ResourceUtils.getFile("classpath:tetris-mp3.mp3")
         val bytes = fileIn.readBytes()
 
         out.write(bytes)
+
+        println("MP3 uploaded")
     }
 
     @RequestMapping("/start-get-time")
     fun startPlaying(response: HttpServletResponse) : Double {
         if(!songTimer.isPlaying) {
+            println("startPlaying being called")
             songTimer.startPlaying()
         }
+        println("Time being requested")
         return songTimer.currentTime()
     }
 }
--- src/main/resources/static/myJs.js
+++ src/main/resources/static/myJs.js
@@ -1,11 +1,21 @@
 var audio = document.getElementById("audio");
 
+var initialPlay = true;
+audio.oncanplaythrough = function() {
+    if(initialPlay) {
+        initialPlay = false;
+        console.log("pode tocar hein")
+        syncTime();
+    }
+}
+
 function syncTime() {
         console.log("play")
         var oReq = new XMLHttpRequest();
         oReq.onload = function() {
             console.log("Current time: "+ oReq.response)
             audio.currentTime = oReq.response;
+            audio.volume = 0.3;
             audio.play();
         };
         oReq.open("get", "http://localhost:8080/start-get-time", true);
